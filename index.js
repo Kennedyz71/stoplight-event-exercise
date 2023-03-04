0a90cdbd1ba9763f567d575155038e57ac8ec107
Kennedy <kennedyz71@gmail.com>
2023-03-03 16:03:40 -0800

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-(function() {
-  'use strict';
+// let lightOn = <textContent> bulb on
 
-  // YOUR CODE HERE
-})();
+// console.log(controls.firstElementChild);
+// console.log(buttonControls.children[0]);
+
+let stopButton = document.getElementById("stopButton");
+let redLight = document.getElementById("stopLight");
+stopButton.addEventListener("click", function () {
+  redLight.classList.toggle("stop");
+  console.log("STOP");
+});
+
+let slowButton = document.getElementById("slowButton");
+let slowLight = document.getElementById("slowLight");
+slowButton.addEventListener("click", function () {
+  slowLight.classList.toggle("slow");
+  console.log("SLOW");
+});
+
+let goButton = document.getElementById("goButton");
+let goLight = document.getElementById("goLight");
+goButton.addEventListener("click", function () {
+  goLight.classList.toggle("go");
+  console.log("GO");
+});
+
+stopButton.addEventListener("mousenter", () => {
+  buttonLog("stop");
+});
+
+let buttons = document.querySelectorAll(".button");
+
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mouseenter", function (event) {
+    // console.log(event);
+    buttonLog(event.target.innerText);
+    // event.target.classList.toggle(event.target.innerText.toLowerCase());
+  });
+}
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mouseleave", function (event) {
+    // console.log(event);
+    buttonLogOut(event.target.innerText);
+  });
+}
+
+redLight.addEventListener("mouseenter", (event) => {
+  redLight.classList.toggle("stop");
+});
+
+function buttonLog(buttonName) {
+  console.log(`Enter ${buttonName} button`);
+}
+
+function buttonLogOut(buttonName) {
+  console.log(`Left ${buttonName} button`);
+}
